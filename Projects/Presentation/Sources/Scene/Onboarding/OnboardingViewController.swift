d6f2db518db658996499b11174d503624a8dbd43
Choyoungjun <cyj070513@gmail.com>
2024-01-24 22:53:47 +0900

    📑 :: 세팅용 ViewController 생성

---
+++ Projects/Presentation/Sources/Scene/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+public class OnboardingViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
