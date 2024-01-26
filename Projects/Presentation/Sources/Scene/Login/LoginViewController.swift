2251cfd0a8a5470128e49694c343073eec801691
Choyoungjun <cyj070513@gmail.com>
2024-01-26 19:25:44 +0900

    ⚡️ :: 로그인 뷰 퍼블리싱

--- Projects/Presentation/Sources/Scene/Login/LoginViewController.swift
+++ Projects/Presentation/Sources/Scene/Login/LoginViewController.swift
@@ -1,11 +1,71 @@
 import UIKit
+
 import SnapKit
 import Then
-import RxFlow
 import RxSwift
 import RxCocoa
-import Core
 
-public class LoginViewController: UIViewController, Stepper {
+import Core
+import DesignSystem
+
+public class LoginViewController: BaseVC<LoginViewModel> {
+    
+    private let disposeBag = DisposeBag()
+    
+    private let pickLabel = UILabel().then {
+        $0.text = "PiCK"
+        $0.textColor = .primary300
+        $0.font = .heading3
+    }
+    private let explainLabel = UILabel().then {
+        $0.text = "스퀘어 계정으로 로그인 해주세요."
+        $0.textColor = .neutral400
+        $0.font = .body2
+    }
+    private let idTextField = PiCKTextField().then {
+        $0.placeholder = "아이디"
+    }
+    private let passwordTextField = PiCKTextField().then {
+        $0.placeholder = "비밀번호"
+        $0.isSecurity = true
+    }
+    private let loginButton = PiCKLoginButton(type: .system).then {
+        $0.isEnabled = false
+    }
+
+    public override func addView() {
+        [
+            pickLabel,
+            explainLabel,
+            idTextField,
+            passwordTextField,
+            loginButton
+        ].forEach { view.addSubview($0) }
+    }
+    public override func setLayout() {
+        pickLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(184)
+            $0.left.equalToSuperview().inset(24)
+        }
+        explainLabel.snp.makeConstraints {
+            $0.top.equalTo(pickLabel.snp.bottom).offset(3)
+            $0.left.equalToSuperview().inset(24)
+        }
+        idTextField.snp.makeConstraints {
+            $0.top.equalTo(explainLabel.snp.bottom).offset(24)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(40)
+        }
+        passwordTextField.snp.makeConstraints {
+            $0.top.equalTo(idTextField.snp.bottom).offset(28)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(40)
+        }
+        loginButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(84)
+            $0.left.right.equalToSuperview().inset(24)
+            $0.height.equalTo(47)
+        }
+    }
 
 }
