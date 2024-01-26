fe4e256849b82597f79ec2805bba7e6b6cbbdf07
Choyoungjun <cyj070513@gmail.com>
2024-01-27 00:12:53 +0900

    📑 :: 메인 뷰 및 뷰 모델 생성

---
+++ Projects/Presentation/Sources/Scene/Main/MainViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
+
+import Core
+import DesignSystem
+
+public class MainViewController: BaseVC<MainViewModel> {
+    
+    public override func attribute() {
+        <#code#>
+    }
+    public override func addView() {
+        <#code#>
+    }
+    public override func setLayout() {
+        <#code#>
+    }
+
+}
---
+++ Projects/Presentation/Sources/Scene/Main/MainViewModel.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+import RxSwift
+import RxCocoa
+import RxFlow
+
+import Core
+import Domain
+
+public class MainViewModel: ViewModelType, Stepper {
+    
+    public var steps = PublishRelay<Step>()
+    
+    public init(steps: PublishRelay<Step> = PublishRelay<Step>()) {
+        self.steps = steps
+    }
+    
+    public struct Input {
+        
+    }
+    public struct Output {
+        
+    }
+    
+    public func transform(input: Input) -> Output {
+        return Output()
+    }
+    
+}
