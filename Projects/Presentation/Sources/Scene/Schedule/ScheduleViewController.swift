ce41b3fbc9af213e1d7fee97a827a07e7c888e24
Choyoungjun <cyj070513@gmail.com>
2024-02-04 02:19:14 +0900

    📑 :: 뷰 생성

---
+++ Projects/Presentation/Sources/Scene/Schedule/ScheduleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScheduleViewController.swift
+//  Presentation
+//
+//  Created by 조영준 on 2/4/24.
+//  Copyright © 2024 com.pick. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
