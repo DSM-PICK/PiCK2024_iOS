61de95e42047d101ce34e0dad9fd45f1c1d18367
Choyoungjun <cyj070513@gmail.com>
2024-02-09 05:36:01 +0900

    ⚡️ :: 공지사항 내용 tableView로 변경

--- Projects/Presentation/Sources/Scene/Notice/Detail/DetailNoticeViewController.swift
+++ Projects/Presentation/Sources/Scene/Notice/Detail/DetailNoticeViewController.swift
@@ -26,34 +26,23 @@ public class DetailNoticeViewController: BaseVC<DetailNoticeViewModel> {
     private let lineView = UIView().then {
         $0.backgroundColor = .neutral900
     }
-    private let contentLabel = UILabel().then {
-        $0.text = """
-        신입생 오리엔테이션 책자에 있는 입학전 과제의 양식입니다.
-        첨부파일을 다운받아 사용하시고,
-        영어와 전공은 특별한 양식이 없으니 내용에 맞게 작성하여 학교 홈
-        페이지
-        에 제출하시기 바랍니다.
-         
-        ■ 과제 제출 마감: 2024년 2월 20일 화요일
-        ■ 학교 홈페이지 학생 회원가입 -> 학교 담당자가 승인
-        ■ 학교 홈페이지 로그인 후 [과제제출 – 신입생 - 각 교과] 게시판
-        에 제출
-        ■ 과제 중 자기소개 PPT는 첨부한 파일을 참고하되, 자유롭게 만들
-        어도
-        됩니다.
-        """
-        $0.textColor = .neutral50
-        $0.font = .caption2
-        $0.numberOfLines = 0
+    private let tableView = UITableView().then {
+        $0.backgroundColor = .white
+        $0.separatorStyle = .none
+        $0.register(DetailNoticeCell.self, forCellReuseIdentifier: DetailNoticeCell.identifier)
     }
     
+    public override func attribute() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
     public override func addView() {
         [
             navigationTitleLabel,
             titleLabel,
             dateLabel,
             lineView,
-            contentLabel
+            tableView
         ].forEach { view.addSubview($0) }
     }
     public override func setLayout() {
@@ -74,10 +63,25 @@ public class DetailNoticeViewController: BaseVC<DetailNoticeViewModel> {
             $0.left.right.equalToSuperview()
             $0.height.equalTo(5)
         }
-        contentLabel.snp.makeConstraints {
-            $0.top.equalTo(lineView.snp.bottom).offset(16)
-            $0.left.right.equalToSuperview().inset(24)
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom)
+            $0.left.right.bottom.equalToSuperview()
         }
+    }
+    
+}
+
+extension DetailNoticeViewController: UITableViewDelegate, UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: DetailNoticeCell.identifier, for: indexPath) as? DetailNoticeCell
+        else {
+            return UITableViewCell()
+        }
+        return cell
     }
     
 }
