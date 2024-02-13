177d3119c04182c49f412b0f5a7d9e93491d04bb
Choyoungjun <cyj070513@gmail.com>
2024-02-13 23:52:47 +0900

    ⚡️ :: PiCKApplyTextView 생성

---
+++ Modules/DesignSystem/Sources/TextView/PiCKApplyTextView.swift
@@ -0,0 +1,21 @@
+//
+//  PiCKApplyTextView.swift
+//  DesignSystem
+//
+//  Created by 조영준 on 2/13/24.
+//  Copyright © 2024 com.pick. All rights reserved.
+//
+
+import UIKit
+
+class PiCKApplyTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
