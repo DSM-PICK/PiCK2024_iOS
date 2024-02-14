780e249778a8964853265b7afcb4ae538e109131
Choyoungjun <cyj070513@gmail.com>
2024-02-15 00:16:47 +0900

    ⚡️ :: 외출증 뷰 퍼블리싱

---
+++ Modules/DesignSystem/Sources/Label/PiCKPaddingLabel.swift
@@ -0,0 +1,21 @@
+//
+//  PiCKPaddingLabel.swift
+//  DesignSystem
+//
+//  Created by 조영준 on 2/14/24.
+//  Copyright © 2024 com.pick. All rights reserved.
+//
+
+import UIKit
+
+class PiCKPaddingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Projects/Presentation/Sources/Scene/OutingPass/OutingPassViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OutingPassViewController.swift
+//  Presentation
+//
+//  Created by 조영준 on 2/14/24.
+//  Copyright © 2024 com.pick. All rights reserved.
+//
+
+import UIKit
+
+class OutingPassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
