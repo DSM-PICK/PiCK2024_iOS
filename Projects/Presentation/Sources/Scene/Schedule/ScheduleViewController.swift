e3a41ea1f99a3d11e0b6fb88b6d90631b736b64b
Choyoungjun <cyj070513@gmail.com>
2024-02-22 02:22:27 +0900

    ⚡️ :: 일정 뷰 퍼블리싱

--- Projects/Presentation/Sources/Scene/Schedule/ScheduleViewController.swift
+++ Projects/Presentation/Sources/Scene/Schedule/ScheduleViewController.swift
@@ -9,72 +9,72 @@ import RxFlow
 import Core
 import DesignSystem
 
-public class ScheduleViewController: BaseVC<ScheduleViewModel> {
+public class ScheduleViewController: BaseVC<ScheduleViewModel>, Stepper {
+    
+    private let disposeBag = DisposeBag()
+    
+    public let steps = PublishRelay<Step>()
     
     private lazy var viewSize = CGRect(
         x: 0,
         y: 0,
         width: self.view.frame.width - 46,
-        height: self.view.frame.height - 184)
+        height: self.view.frame.height - 184
+    )
     
-    private lazy var segementedTimetableView = ScrollTimeTableView(frame: viewSize)
-    private let segementedCalendarView = AcademicCalendarView()
-    
+    private lazy var segmentedTimetableView = ScrollTimeTableView(frame: viewSize)
+    private lazy var segmentedCalendarView = AcademicScheduleView(frame: viewSize)
     private let navigationTitleLabel = UILabel().then {
         $0.text = "일정"
         $0.textColor = .neutral50
         $0.font = .subTitle3M
     }
-    private lazy var segementedControl = UISegmentedControl(items: [
+    private lazy var segmentedControl = PiCKSegmentedControl(items: [
         "시간표",
         "학사일정"
-    ]).then {
-        $0.selectedSegmentIndex = 0
-    }
+    ])
     
     public override func attribute() {
         navigationItem.titleView = navigationTitleLabel
-        navigationItem.hidesBackButton = false
+    }
+    public override func bind() {
+        self.segmentedControl.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
+        self.didChangeValue(segment: self.segmentedControl)
     }
     public override func addView() {
         [
-            segementedControl,
-            segementedTimetableView,
-            segementedCalendarView
+            segmentedControl,
+            segmentedTimetableView,
+            segmentedCalendarView
         ].forEach { view.addSubview($0) }
     }
     public override func setLayout() {
-        segementedControl.snp.makeConstraints {
+        segmentedControl.snp.makeConstraints {
             $0.top.equalToSuperview().inset(90)
             $0.left.right.equalToSuperview().inset(24)
             $0.height.equalTo(36)
         }
-        segementedTimetableView.snp.makeConstraints {
-            $0.top.equalTo(segementedControl.snp.bottom).offset(28)
+        segmentedTimetableView.snp.makeConstraints {
+            $0.top.equalTo(segmentedControl.snp.bottom).offset(28)
             $0.left.right.equalToSuperview().inset(23)
             $0.bottom.equalToSuperview().inset(74)
         }
-        segementedCalendarView.snp.makeConstraints {
-            $0.top.equalTo(segementedControl.snp.bottom).offset(28)
+        segmentedCalendarView.snp.makeConstraints {
+            $0.top.equalTo(segmentedControl.snp.bottom).offset(28)
             $0.left.right.equalToSuperview().inset(23)
             $0.bottom.equalToSuperview().inset(74)
         }
-        
-        self.segementedControl.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
-        self.didChangeValue(segment: self.segementedControl)
     }
-       
-
-     
-     @objc private func didChangeValue(segment: UISegmentedControl) {
-       self.shouldHideFirstView = segment.selectedSegmentIndex != 0
-     }
+    
+    @objc private func didChangeValue(segment: UISegmentedControl) {
+        self.shouldHideFirstView = segment.selectedSegmentIndex != 0
+    }
     
     var shouldHideFirstView: Bool? {
-      didSet {
-        guard let shouldHideFirstView = self.shouldHideFirstView else { return }
-        self.segementedTimetableView.isHidden = shouldHideFirstView
-        self.segementedCalendarView.isHidden = !self.segementedTimetableView.isHidden
-      }
+        didSet {
+            guard let shouldHideFirstView = self.shouldHideFirstView else { return }
+            self.segmentedTimetableView.isHidden = shouldHideFirstView
+            self.segmentedCalendarView.isHidden = !self.segmentedTimetableView.isHidden
+        }
     }
 }
