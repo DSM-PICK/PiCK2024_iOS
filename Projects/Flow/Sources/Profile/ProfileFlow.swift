eea60604ac14ba582cb5bb06beb9f63d9b2b4a43
Choyoungjun <cyj070513@gmail.com>
2024-02-22 23:51:34 +0900

    ⚡️ :: AlertRequired 추가

--- Projects/Flow/Sources/Profile/ProfileFlow.swift
+++ Projects/Flow/Sources/Profile/ProfileFlow.swift
@@ -3,6 +3,7 @@ import UIKit
 import RxFlow
 
 import Core
+import DesignSystem
 import Presentation
 
 public class ProfileFlow: Flow {
@@ -23,6 +24,8 @@ public class ProfileFlow: Flow {
         switch step {
             case .profileRequired:
                 return navigateToProfile()
+            case .logoutAlertRequired:
+                return presentLogoutAlert()
             default:
                 return .none
         }
@@ -31,8 +34,22 @@ public class ProfileFlow: Flow {
     private func navigateToProfile() -> FlowContributors {
         return .one(flowContributor: .contribute(
             withNextPresentable: rootViewController,
-            withNextStepper: rootViewController.viewModel
+            withNextStepper: rootViewController//.viewModel
         ))
+    }
+    
+    private func presentLogoutAlert() -> FlowContributors {
+        let logoutAlert = PiCKAlert(
+            questionText: "정말 로그아웃 하시겠습니까?",
+            cancelButtonTitle: "아니요",
+            checkButtonTitle: "예",
+            clickToAction: {
+                print("로그아웃")
+            })
+        logoutAlert.modalPresentationStyle = .overFullScreen
+        logoutAlert.modalTransitionStyle = .crossDissolve
+        rootViewController.present(logoutAlert, animated: true)
+        return .none
     }
 
 }
