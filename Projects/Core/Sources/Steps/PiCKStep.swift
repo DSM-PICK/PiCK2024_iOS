4bf1f8e41217b08221e62f1b319c13d56a665615
Choyoungjun <cyj070513@gmail.com>
2024-02-22 23:48:53 +0900

    ⚡️ :: Step 추가 및 정리

--- Projects/Core/Sources/Steps/PiCKStep.swift
+++ Projects/Core/Sources/Steps/PiCKStep.swift
@@ -1,12 +1,37 @@
 import RxFlow
 
 public enum PiCKStep: Step {
+    
     case onBoardingRequired
     case loginRequired
+    
+    //main
     case mainRequired
+    case outingPassRequired
+    
+    //schedule
     case scheduleRequired
+    
+    //apply
     case applyRequired
+    case classroomMoveApplyRequired
+    case outingApplyRequired
+    case earlyLeaveApplyRequired
+    case timePickerAlertRequired(time: [String])
+    
+    //schoolMeal
     case schoolMealRequired
+    
+    //profile
     case profileRequired
+    case logoutAlertRequired
+    case logoutRequired
+    
+    //notice
+    case noticeRequired
+    case detailNoticeRequired
+    
+    //test
     case testRequired
+
 }
