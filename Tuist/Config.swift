d608341752173e418794775feb891c4e1a01d309
Choyoungjun <cyj070513@gmail.com>
2024-02-27 00:19:55 +0900

    ⚙️ :: Config 파일 경로 추가

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,6 +2,7 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
+        .local(path: .relativeToManifest("../../Plugins/ConfigurationPlugin")),
         .local(path: .relativeToManifest("../../Plugins/DependencyPlugin")),
         .local(path: .relativeToManifest("../../Plugins/EnvironmentPlugin"))
     ]
