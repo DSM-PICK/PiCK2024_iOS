4bab03c0f49507368d98952a1ba18db74b8d34b5
Choyoungjun <cyj070513@gmail.com>
2024-03-02 03:48:25 +0900

    ⚡️ :: 로고 이미지 추가

--- Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
+++ Projects/Modules/DesignSystem/Sources/Extension/Image/UIImage+.swift
@@ -1,13 +1,13 @@
 import UIKit
 
 public extension UIImage {
+    //MARK: Logo
+    static let PiCKLogo = DesignSystemAsset.Image.pickLogo.image
+    
     //MARK: onboarding
     static let onboardingImage1 = DesignSystemAsset.Image.onboarding1.image
     static let onboardingImage2 = DesignSystemAsset.Image.onboarding2.image
     static let onboardingImage3 = DesignSystemAsset.Image.onboarding3.image
-    
-    //MARK: rectangle
-    static let pageControlRectangle = DesignSystemAsset.Image.pageControlRectangle.image
     
     //MARK: icon
     static let eyeOn = DesignSystemAsset.Image.eyeOn.image
