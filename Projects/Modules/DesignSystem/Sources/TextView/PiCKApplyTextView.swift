e4a48d7a9fbd8b991c84c1000d2ea0b95e1d1838
Choyoungjun <cyj070513@gmail.com>
2024-03-02 03:45:13 +0900

    ♻️ :: PlaceholderText가 인식되던 문제 해결

--- Projects/Modules/DesignSystem/Sources/TextView/PiCKApplyTextView.swift
+++ Projects/Modules/DesignSystem/Sources/TextView/PiCKApplyTextView.swift
@@ -9,41 +9,37 @@ public class PiCKApplyTextView: UITextView {
     
     private let disposeBag = DisposeBag()
     
+    private let placeholderLabel = UILabel().then {
+        $0.text = "내용을 입력해주세요"
+        $0.textColor = .neutral500
+        $0.font = .caption2
+        $0.isHidden = false
+    }
+    
     public override init(frame: CGRect, textContainer: NSTextContainer?) {
         super.init(frame: frame, textContainer: textContainer)
         setup()
         bind()
+        layout()
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     private func bind() {
-        self.rx.text.orEmpty
-            .bind { [weak self] text in
-                if text == "내용을 입력해주세요" {
-                    self?.textColor = .neutral500
-                } else {
-                    self?.textColor = .neutral50
-                }
-            }.disposed(by: disposeBag)
-        
         self.rx.didBeginEditing
             .bind { [weak self] in
-                if self?.text == "내용을 입력해주세요" {
-                    self?.text = ""
-                }
+                self?.placeholderLabel.isHidden = true
             }.disposed(by: disposeBag)
         
         self.rx.didEndEditing
             .bind { [weak self] in
                 if self?.text.isEmpty == true {
-                    self?.text = "내용을 입력해주세요"
+                    self?.placeholderLabel.isHidden = false
                 }
             }.disposed(by: disposeBag)
     }
     private func setup() {
-        self.text = "내용을 입력해주세요"
         self.textColor = .neutral50
         self.font = .caption2
         self.textContainer.maximumNumberOfLines = 0
@@ -52,6 +48,14 @@ public class PiCKApplyTextView: UITextView {
         self.autocorrectionType = .no
         self.autocapitalizationType = .none
         self.textContainerInset = UIEdgeInsets(top: 14, left: 16, bottom: 16, right: 16)
+    }
+    private func layout() {
+        self.addSubview(placeholderLabel)
+        
+        placeholderLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(14)
+            $0.left.equalToSuperview().inset(16)
+        }
     }
     
 }
