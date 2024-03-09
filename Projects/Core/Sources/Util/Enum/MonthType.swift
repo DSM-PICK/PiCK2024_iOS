080d3bf9e0c98078bacdcfaf57ee7ccd2875ff40
Choyoungjun <cyj070513@gmail.com>
2024-03-09 21:39:00 +0900

    ⚡️ :: MonthType 추가

---
+++ Projects/Core/Sources/Util/Enum/MonthType.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public enum MonthType: String {
+    case january = "JANUARY"
+    case february = "FEBRUARY"
+    case march = "MARCH"
+    case april = "APRIL"
+    case may = "MAY"
+    case june = "JUNE"
+    case july = "JULY"
+    case august = "AUGUST"
+    case september = "SEPTEMBER"
+    case october = "OCTOBER"
+    case november = "NOVEMBER"
+    case december = "DECEMBER"
+}
