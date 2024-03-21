1b8d58e5fbd01b97198a7e0da5e813c2d0bd8bc8
Choyoungjun <cyj070513@gmail.com>
2024-03-22 00:51:35 +0900

    ⚡️ :: 설명 추가

--- Projects/Core/Sources/Util/Enum/DateFormatIndicated.swift
+++ Projects/Core/Sources/Util/Enum/DateFormatIndicated.swift
@@ -1,13 +1,22 @@
 import Foundation
 
 public enum DateFormatIndicated: String {
+    ///M월 d일 (E)
     case dateAndDays = "M월 d일 (E)"
+    ///yyyy-MM-dd
     case fullDate = "yyyy-MM-dd"
+    ///yyyy년 MM월
     case yearsAndMonthKor = "yyyy년 MM월"
+    ///yyyy
     case year = "yyyy"
+    ///MMMM
     case fullMonth = "MMMM"
+    ///M월
     case month = "M월"
+    ///d
     case day = "d"
+    ///M월 d일
     case monthAndDay = "M월 d일"
+    ///yyyy년 M월 d일
     case fullDateKor = "yyyy년 M월 d일"
 }
