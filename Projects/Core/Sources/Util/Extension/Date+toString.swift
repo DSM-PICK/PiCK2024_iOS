b99cefc155c705302800de4d65e7d5441b53836e
Choyoungjun <cyj070513@gmail.com>
2024-03-22 00:51:23 +0900

    ♻️ :: 수정

--- Projects/Core/Sources/Util/Extension/Date+toString.swift
+++ Projects/Core/Sources/Util/Extension/Date+toString.swift
@@ -1,11 +1,17 @@
 import Foundation
 
 public extension Date {
-    func toString(_ formate: String) -> String {
+    func toString(type: DateFormatIndicated) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = formate
+        dateFormatter.dateFormat = type.rawValue
         dateFormatter.locale = Locale(identifier: "ko_kr")
         dateFormatter.timeZone = TimeZone(identifier: "KST")
+        return dateFormatter.string(from: self)
+    }
+    
+    func toStringEng(type: DateFormatIndicated) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = type.rawValue
         return dateFormatter.string(from: self)
     }
 }
