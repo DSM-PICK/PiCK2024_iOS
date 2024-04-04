53ad3b59f2c7cd7c9eca63530f4ae29a760749d5
cyj513 <cyj070513@gmail.com>
2024-04-04 23:11:36 +0900

    ♻️ :: 정리

--- Projects/Flow/Sources/Main/MainFlow.swift
+++ Projects/Flow/Sources/Main/MainFlow.swift
@@ -21,6 +21,8 @@ public class MainFlow: Flow {
         guard let step = step as? PiCKStep else { return .none }
         
         switch step {
+            case .onBoardingRequired:
+                return .end(forwardToParentFlowWithStep: PiCKStep.onBoardingRequired)
             case .mainRequired:
                 return navigateToMain()
             case .profileRequired:
--- Projects/Flow/Sources/Profile/ProfileFlow.swift
+++ Projects/Flow/Sources/Profile/ProfileFlow.swift
@@ -31,7 +31,6 @@ public class ProfileFlow: Flow {
                 return navigateToProfile()
             case .logoutRequired:
                 return .end(forwardToParentFlowWithStep: PiCKStep.onBoardingRequired)
-//                return logout()
             default:
                 return .none
         }
@@ -44,45 +43,4 @@ public class ProfileFlow: Flow {
         ))
     }
     
-//    private func presentLogoutAlert() -> FlowContributors {
-//        let logoutAlert = PiCKAlert(
-//            questionText: "정말 로그아웃 하시겠습니까?",
-//            cancelButtonTitle: "아니요",
-//            checkButtonTitle: "예",
-//            clickToAction: {
-//                print("fjdskl")
-//            }
-//        )
-//        logoutAlert.modalPresentationStyle = .overFullScreen
-//        logoutAlert.modalTransitionStyle = .crossDissolve
-//        rootViewController.present(logoutAlert, animated: true)
-//        return .none
-//    }
-    
-//    private func logout() -> FlowContributors {
-////        let dd = OnboardingViewController(viewModel: container.onboardingViewModel)
-////            UIView.transition(
-////                with: self.rootViewController.view.window!,
-////                duration: 0.5,
-////                options: .transitionCrossDissolve) {
-//////                    self.rootViewController.dismiss(animated: true)
-//////                    self.rootViewController.navigationController?.popToViewController(dd, animated: true)
-////                }
-//        let schoolMealFlow = LoginFlow()
-//        Flows.use(schoolMealFlow, when: .created) { [weak self] root in
-//            self?.rootViewController.pushViewController(root, animated: true)
-//        }
-//        
-//        return .one(flowContributor: .contribute(
-//            withNextPresentable: schoolMealFlow,
-//            withNextStepper: OneStepper(withSingleStep: PiCKStep.loginRequired)
-//        ))
-//        return .one(flowContributor: .contribute(
-//            withNextPresentable: loginFlow,
-//            withNextStepper: OneStepper(withSingleStep: PiCKStep.loginRequired)
-//        ))
-//        return .end(forwardToParentFlowWithStep: PiCKStep.loginRequired)
-
-//    }
-
 }
